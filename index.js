b4c07a026d5dcd596c7c0c24574937d532f82b41
techux <connect.world12345@gmail.com>
2025-03-13 04:18:22 +0530

    Fixed views path for vercel

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT;
 
+
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
